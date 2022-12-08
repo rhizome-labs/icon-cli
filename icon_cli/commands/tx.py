e33f5b25a1285f27eeeaf9c8a9452059452065b2
Brian Li <53635700+rhizome-labs@users.noreply.github.com>
2022-12-08 18:22:48 +0900

    Add config, query, and tx commands.

---
+++ icon_cli/commands/tx.py
@@ -0,0 +1,6 @@
+import typer
+from rich.pretty import pprint
+
+from icon_cli.utils import Utils
+
+app = typer.Typer()
--- icon_cli/main.py
+++ icon_cli/main.py
@@ -1,12 +1,11 @@
 import typer
 
-from icon_cli.commands import config
+from icon_cli.commands import config, query, tx
 
+# Initialize Typer.
 app = typer.Typer()
 
+# Add config, query, and tx commands.
 app.add_typer(config.app, name="config")
-
-
-@app.command()
-def hello():
-    print("HELLO!")
+app.add_typer(query.app, name="query")
+app.add_typer(tx.app, name="tx")
